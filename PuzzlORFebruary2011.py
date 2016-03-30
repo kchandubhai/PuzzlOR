e7496e6f620746984b73914a08e44d3f21fd02f2
Edima Udo <edimaudo@gmail.com>
2016-03-29 23:57:04 -0400

    Solution for PuzzlORFebruary2011.py

--- PuzzlORFebruary2011.py
+++ PuzzlORFebruary2011.py
@@ -1,3 +1,34 @@
 # PuzzlOR February 2011
 # Best Host
 
+
+
+def main():
+    people = ["Andrew","Betty","Cara","Dave","Erica","Frank"]
+    arrangement = []
+
+    validArrangement= {
+    "Andrew":["Dave","Andrew","Frank"],
+    "Betty":["Cara","Betty","Erica"],
+    "Cara":["Betty","Cara","Frank"],
+    "Dave":["Andrew","Dave","Erica"],
+    "Erica":["Betty","Erica","Dave"],
+    "Frank":["Andrew","Frank","Cara"]
+    }
+
+    templist = []
+    templist.extend(validArrangement["Andrew"])
+    while len(templist) != len(people):
+        temp = validArrangement[templist[0]]
+        for value in temp:
+            if value not in templist:
+                templist.insert(0,value)
+    print(templist)
+        
+    
+
+#print(validArrangement[templist[0]])
+#print(templist)
+#print(validArrangement["Andrew"])
+
+main()
