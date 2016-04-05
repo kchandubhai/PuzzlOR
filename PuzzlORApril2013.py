a5c9f0b67edb90abb6695702b7b5a8de650adc6b
Edima Udo <edimaudo@gmail.com>
2016-04-05 00:13:23 -0400

    Created PuzzlORApril2013.py

---
+++ PuzzlORApril2013.py
@@ -0,0 +1,17 @@
+# PuzzlOR April 2013 Subs and Battleship
+# Subs vs. Battleships - 30.78
+
+import itertools
+import random
+import math
+import operator
+
+def euclidean_distance(a,b):
+    c = [math.pow(a[i] - b[i],2) for i in range(len(a))]
+    return math.sqrt(sum(c))
+def find_closest():
+	
+def main():
+
+
+main()
