8fc333f85e4c58df1c143bef2250b58d8b2327b2
Edima <edimaudo@gmail.com>
2016-04-13 16:14:29 -0400

    Create PuzzlORFebruary2010.py

---
+++ PuzzlORFebruary2010.py
@@ -0,0 +1,15 @@
+#PuzzlOR February 2010 Planet Colonisation
+# area 19
+
+import math
+import operator
+
+def calc_distance(position,area):
+    resources = []
+
+
+landingPosition = [1,2,3,4,5,6,7,8,9,10,1,12,13,14,15,16,17,18,19,20]
+
+distanceCalc = []
+
+for position in landingPosition:
