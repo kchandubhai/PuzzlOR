512f2a225e029867726526ec0a1b439c377d8af4
Edima <edimaudo@gmail.com>
2016-04-14 16:34:10 -0400

    Update PuzzlORApril2012.py

--- PuzzlORApril2012.py
+++ PuzzlORApril2012.py
@@ -1,35 +1,57 @@
 # PuzzlOR April 2012 McEverywhere
 # 3
 
+import math
+import itertools
+
 def generate_coordinate(value,alist):
     tempList = []
     tempVal = value[0]
     tempNum = int(value[1])
     tempVal1 = int(alist.index(tempVal)) + 1
-    tempList.append([tempVal1,tempNum])
+    tempList.extend([tempVal1,tempNum])
     return tempList
-    
 
 def generate_area(xVal,yVal):
-    templist = []
-    for x in xVal:
-        for y in yVal:
-            templist.append(str(x) + str(y))
+    templist = [str(x) + str(y) for x in xVal for y in yVal]
     return templist
 
 def euclidean_distance(a,b):
     c = [math.pow(a[i] - b[i],2) for i in range(len(a))]
-    return math.sqrt(sum(c))
+    return round(math.sqrt(sum(c)),2)
+
+def calc_distance(position,homeinfo,xdata):
+    if len(position) < 2:
+        area =  [euclidean_distance(generate_coordinate(i,xdata), \
+        generate_coordinate(position[0],xdata)) for  i in homeinfo]
+    else:
+        value = ""
+
+    outputList = [i for i in area if i <= 4]
+    if len(outputList) == len(homeinfo):
+        return True
+    return False
+
+
 
 
 def main():
-    xVal = ['A',"B","C","D","E","F","G","H","I"]
+    xVal = ['A',"B","C","D","E","F","G","H","I","J"]
     yVal = [1,2,3,4,5,6,7,8,9,10]
     homeLocation = ["A4","A8","A9","A10","B4","B7",
     "C8","C9","D2","E1","F2","F5","F6","G5","G9",
     "H3","H5","I5","I9","J7"]
-    #print(generate_area(xVal,yVal))
-    #print(len(homeLocation))
-    #print(generate_coordinate("B2",xVal))
+
+   
+    
+    outputData = []
+    area = generate_area(xVal,yVal)
+    for val in itertools.combinations(area,1):
+        position = list(val)
+        outputData.append(calc_distance(position,homeLocation,xVal))
+    print(outputData)
+    #print(min(outputData))
+
+
 
 main()
