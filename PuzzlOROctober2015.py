7774c747af85ada1e3ff8ea701fb4526b3db1721
Edima <edimaudo@gmail.com>
2016-04-14 12:50:47 -0400

    Update PuzzlOROctober2015.py

--- PuzzlOROctober2015.py
+++ PuzzlOROctober2015.py
@@ -1,14 +1,20 @@
 # PuzzlOR October 2015
-# BABC
+
 import math
 import operator
 import itertools
-
 
 def euclidean_distance(a,b):
     c = [math.pow(a[i] - b[i],2) for i in range(len(a))]
     return round(math.sqrt(sum(c)),2)
 
+def generate_data(config,configData):
+    return [configData[i] for i in config]
+
+def find_closest(config,sampleList,configData):
+    output = [[euclidean_distance(generate_data(value[0],configData),generate_data(config,configData)),value[1],config] for value in sampleList]
+    sorted_output = sorted(output, key=operator.itemgetter(0))
+    return sorted_output[0]
 
 sampleRaceCar = [
 [['AE','AT','ATR','CB'],12],
@@ -38,7 +44,22 @@ configurationData = {
 "CB":3
 }
 
-allConfiguration = ["AE","BE","CE","AT","BT","CT","ATr","BTr","CTr","AB","BB","CB"]
-for configuration in itertools.combinations(allConfiguration,4):
-    config = list(configuration)
-    
+#config = ["AE","BE","AE","AT"]
+#print(find_closest(config,sampleRaceCar,configurationData))
+
+outputList = []
+engines = ["AE","BE","CE"]
+tires = ["AT","BT","CT"]
+transmissions = ["ATR","BTR","CTR"]
+brakes = ["AB","BB","CB"]
+allConfiguration = ["AE","BE","CE","AT","BT","CT","ATR","BTR","CTR","AB","BB","CB"]
+
+for engine in engines:
+    for tire in tires:
+        for transmission in transmissions:
+            for brake in brakes:
+                config = [engine,tire,transmission,brake]
+                outputList.append(find_closest(config,sampleRaceCar,configurationData))
+sorted_outputList = sorted(outputList,key=operator.itemgetter(1))
+
+print(sorted_outputList)
