41b9c40cc360ae214eb91d360b12bfd7883700e6
Edima Udo <edimaudo@gmail.com>
2016-04-15 19:45:27 -0400

    Solution

--- PuzzlORApril2012.py
+++ PuzzlORApril2012.py
@@ -44,7 +44,6 @@ def calc_distance(position,homeinfo,xdata):
     else:
         outputData = []
         for i in position:
-<<<<<<< Updated upstream
             area = [euclidean_distance(generate_coordinate(i,xdata),generate_coordinate(home,xdata)) \
             for home in homeinfo]
             outputData.append(area)
@@ -53,13 +52,10 @@ def calc_distance(position,homeinfo,xdata):
     if len(outputList) == len(homeinfo):
         return position
     return None
-=======
-            tempOutput = [euclidean_distance(generate_coordinate(i,xdata),
-                                             generate_coordinate(home,xdata)) \
-            for home in homeinfo]
-            outputData.append(tempOutput)
+    tempOutput = [euclidean_distance(generate_coordinate(i,xdata),generate_coordinate(home,xdata)) for home in homeinfo]
+    outputData.append(tempOutput)
         
-        return compare_list(outputData)
+    return compare_list(outputData)
         ##print(outputData)
 
 
@@ -70,19 +66,19 @@ def calc_distance(position,homeinfo,xdata):
     #return None
 
 
->>>>>>> Stashed changes
-
-def calc_min(alist,blist):
-    output = [min(alist[i],blist[i]) for i in range(len(blist))]
-    return output
-    
-def compare_list(alist):
-    countval = 0
-    tempList = []
-    while countval < len(alist)-1:
-        tempList = calc_min(tempList,alist[countval])
-    return tempList
-    
+##>>>>>>> Stashed changes
+##
+##def calc_min(alist,blist):
+##    output = [min(alist[i],blist[i]) for i in range(len(blist))]
+##    return output
+##    
+##def compare_list(alist):
+##    countval = 0
+##    tempList = []
+##    while countval < len(alist)-1:
+##        tempList = calc_min(tempList,alist[countval])
+##    return tempList
+##    
     
 
 
