2267f3f8caccd02b2928669fa76eadac658387cb
Edima <edimaudo@gmail.com>
2016-04-18 16:48:09 -0400

    Update PuzzlORApril2013.py

--- PuzzlORApril2013.py
+++ PuzzlORApril2013.py
@@ -4,8 +4,19 @@ import random
 import math
 import operator
 
-def generate_random(alist):
-    return 
+def get_surrouding_position(position, positionInfo):
+    tempval = position[0]
+    tempnum = int(position[1:])
+    templistnum = []
+    templistval = positionInfo[tempval]
+    if tempnum == 1:
+        templistnum.extend([tempnum,tempnum+1])
+    elif tempnum in [2,3,4,5,6,7,8,9]:
+        templistnum.extend([tempnum-1,tempnum,tempnum+1])
+    elif tempnum == 10:
+        templistnum.extend([tempnum-1,tempnum])
+    finalist = [val + str(num) for val in templistval for num in templistnum]
+    return finalist
 
 def generate_coordinate(value,alist):
     tempList = []
@@ -13,7 +24,7 @@ def generate_coordinate(value,alist):
     return tempList
 
 def euclidean_distance(a,b):
-    c = [math.pow(a[i] - b[i],2) 
+    c = [math.pow(a[i] - b[i],2)
     for i in range(len(b))]
     return round(math.sqrt(sum(c)),2)
 
@@ -21,38 +32,50 @@ def update_battleship(alist,blist):
     if len(blist) < 1:
         return alist
     else:
-        templist = [value for value in alist 
-        if value not in blist]    
+        templist = [value for value in alist
+        if value not in blist]
         return templist
 
 def assign_sub(value,alist,blist):
     calcList = [[euclidean_distance(
     generate_coordinate(val,blist),
-    generate_coordinate(value,blist)),val] 
-    for val in alist]   
+    generate_coordinate(value,blist)),val]
+    for val in alist]
     sorted_calcList = sorted(calcList,
                              key=operator.itemgetter(1))
     return sorted_calcList[0]
 
 def main():
     xVal = ["A","B","C","D","E","F","G","H","I","J"]
+    positionInfo = {
+    "A":["A","B"],
+    "B":["A","B","C"],
+    "C":["B","C","D"],
+    "D":["C","D","E"],
+    "E":["D","E","F"],
+    "F":["E","F","G"],
+    "G":["F","G","H"],
+    "H":["G","H","I"],
+    "I":["H","I","J"],
+    "J":["I","J"]
+    }
     subs = ["A1","B2","B6","D2","D3","D7","F10",
     "G1","G4","I1","I5","I7","J2","J3","J6"]
-    
+
     battleships = ["B1","C4","D9","F1","F2",
     "F3","F5","F8","G5","G6","G8",
     "H7","J1","J4","J10"]
 
+##    tempBattleShips = battleships
+##    assignedSub = []
+##    distance_calc = 0
+##    for sub in subs:
+##        tempBattleShips = update_battleship(tempBattleShips,assignedSub)
+##        temp = assign_sub(sub,tempBattleShips,xVal)
+##        assignedSub.append(temp[1])
+##        distance_calc += temp[0]
+##    print(distance_calc)
 
-    tempBattleShips = battleships
-    assignedSub = []
-    distance_calc = 0
-    for sub in subs:
-        tempBattleShips = update_battleship(tempBattleShips,assignedSub)
-        temp = assign_sub(sub,tempBattleShips,xVal)
-        assignedSub.append(temp[1])
-        distance_calc += temp[0]
-    print(distance_calc)
 
-   
+
 main()
