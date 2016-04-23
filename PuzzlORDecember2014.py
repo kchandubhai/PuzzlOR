f52012b7793fe9c43a42c386563df32ef89ef564
Edima Udo <edimaudo@gmail.com>
2016-04-23 16:47:26 -0400

    Update

--- PuzzlORDecember2014.py
+++ PuzzlORDecember2014.py
@@ -1,6 +1,5 @@
 # PuzzlOR December 2014 Electrifying
 
-import random
 import math
 import itertools
 import string
@@ -14,12 +13,9 @@ def generate_distance(substation,neighborhood,yVal):
     distanceCalc = 0
     templist = []
     templist2 = []
-    substation0 = substation[0]
-    substation1 = substation[1]
-    substation2 = substation[2]
-    substation0position = generate_coordinate(substation0,yVal)
-    substation1position = generate_coordinate(substation1,yVal)
-    substation2position = generate_coordinate(substation2,yVal)
+    substation0position = generate_coordinate(substation[0],yVal)
+    substation1position = generate_coordinate(substation[1],yVal)
+    substation2position = generate_coordinate(substation[2],yVal)
     for area in neighborhood:
         areaCalc = generate_coordinate(area,yVal)
         templist = [
@@ -27,29 +23,20 @@ def generate_distance(substation,neighborhood,yVal):
         round(euclidean_distance(areaCalc,substation1position),2),
         round(euclidean_distance(areaCalc,substation2position),2)
         ]
+        distanceCalc += min(templist) 
         templist2.append(find_min(templist))
-        print(templist)
-        #print(templist2)
-        distanceCalc += min(templist)
-        #print(distanceCalc)
-    print(templist2)
     if len(set(templist2)) != 3:
         distanceCalc = 10000
-    return round(distanceCalc,2)
+    return distanceCalc
 
 def generate_coordinate(value,alist):
-    tempList = []
-    tempVal = value[0]
-    tempNum = int(value[1]) - 1
-    tempVal1 = int(alist.index(tempVal)) + 1
-    tempList.extend([tempVal1,tempNum])
-    return tempList
-
+    return [int(alist.index(value[0])) + 1,int(value[1:])]
+    
 def generate_area(xVal,yVal):
     return [str(y) + str(x) for x in xVal for y in yVal]
 
 def euclidean_distance(a,b):
-    c = [math.pow(a[i] - b[i],2) for i in range(len(a))]
+    c = [math.pow(a[i] - b[i],2) for i in range(len(b))]
     return math.sqrt(sum(c))
 
 def main():
@@ -59,22 +46,14 @@ def main():
                      "C5","C10","D9","E2","E6",
                      "E8","F3","F5","G8","G9",
                      "H3","H5","H7","H8","J4"]
-    #print(generate_coordinate("E6",yVal))
-    #[(1, 2), (0, 6), (8, 3)]
-#    ["C2","G1","D9"]
+
     allList = generate_area(xVal,yVal)
-    countlist = []
-    tempval = generate_distance(["B1","F1","H4"], neighborhoods, yVal)
-    countlist.append(tempval)
+    countlist = []    
+    for substations in itertools.combinations(allList,3):
+         tempval = generate_distance(list(substations), neighborhoods, yVal)
+         countlist.append(tempval)
     output = list(set(countlist))
     output.sort()
     print(output[0])
-    
-#    for substations in itertools.combinations(allList,3):
-#         tempval = generate_distance(list(substations), neighborhoods, yVal)
-#         countlist.append(tempval)
-#    output = list(set(countlist))
-#    output.sort()
-#    print(output[0])
 
 main()
