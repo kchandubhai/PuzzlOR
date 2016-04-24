ce24fe6f9b18c154fa5d09f4193377bce52de2c3
Edima Udo <edimaudo@gmail.com>
2016-04-24 13:59:35 -0400

    Update

--- PuzzlORFebruary2009.py
+++ PuzzlORFebruary2009.py
@@ -10,13 +10,8 @@ def manhattan_distance(a,b):
     return sum([abs(a[i] - b[i]) for i in range(len(b))])
 
 def generate_coordinate(value,alist):
-    tempList = []
-    tempVal = value[0]
-    tempNum = int(value[1])
-    tempVal1 = int(alist.index(tempVal)) + 1
-    tempList.extend([tempVal1,tempNum])
-    return tempList
-
+    return [int(alist.index(value[0])) + 1,int(value[1])]
+    
 def generate_area(xVal,yVal):
     return [str(x) + str(y) for x in xVal for y in yVal]
 
@@ -37,6 +32,4 @@ def main():
     possibileArea = [i for i in area if i not in factories and i not in towns]
     possibleAreaCapacity = 1000
 
-    for value in possibileArea:
-        factoriesData = factories
-        factoriesData.append(value)
+    c
