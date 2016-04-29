b964a078d9893ab94baba8cee5d5ffe9af56747c
Edima <edimaudo@gmail.com>
2016-04-29 16:57:35 -0400

    Update PuzzlOROctober2009.py

--- PuzzlOROctober2009.py
+++ PuzzlOROctober2009.py
@@ -1,28 +1,62 @@
 ## puzzlOR October 2009
 ## Connected and infected
-# 21
-def main():
-    world = {
-    "blue":["blue","violet","red"],
-    "violet":["violet","blue","green","yellow"],
-    "red":["red","blue"],
-    "yellow":["yellow","violet"],
-    "green":["green","violet"]
-    }
+## 21
 
-    constval = 10**9
-    currentBlue = 0.5 * constval
-    currentRed = 0.5 * constval
-    currentViolet = 4 * constval
-    currentYellow = 1 * constval
-    currentGreen = 0.5 * constval
-    currentPopulation = currentBlue + currentRed + currentViolet + currentYellow
-    + currentGreen
+def update_area(areaData,areaScore,infectedArea):
+    areaInfo = []
+    for value in infectedArea:
+        areaInfo.extend(areaData[value])
+    areaInfo = list(set(areaInfo))
+    for value in areaScore:
+        if value[0] in areaInfo:
 
-##    print(currentPopulation)
-##    infectionCount = 0
-##    monthCount = 0
-##    while infectionCount < 0.5*currentPopulation:
-##        dosomething
 
-main()
+
+
+
+
+
+
+
+
+# add areascore to area data
+areaData = {
+"blue":["blue","violet","red"],
+"violet":["violet","blue","green","yellow"],
+"red":["red","blue"],
+"yellow":["yellow","violet"],
+"green":["green","violet"]
+}
+
+areaScore = [["blue",10000],["violet",0],["red",0],["yellow",0],["green",0]]
+
+constval = 10**9
+currentBlue = 0.5 * constval
+currentRed = 0.5 * constval
+currentViolet = 4 * constval
+currentYellow = 1 * constval
+currentGreen = 0.5 * constval
+currentPopulation = currentBlue + currentRed + currentViolet + currentYellow
++ currentGreen
+
+infectedArea = ["blue"]
+
+monthCount = 0
+populationInfected = 0
+
+print(update_area(areaData,areaScore,infectedArea))
+
+while populationInfected < 0.5*currentPopulation:
+
+    output = update_area(areaData,areaScore,infectedArea)
+    populationInfected = output[0]
+    infectedArea = output[1]
+    print(populationInfected)
+    monthCount+=1
+    break
+print(monthCount)
+
+
+
+
+
