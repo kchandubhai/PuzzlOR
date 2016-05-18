81dc552b047b37dea1d319f27114469ba1bec7c1
Edima Udo <edimaudo@gmail.com>
2016-05-17 20:41:39 -0400

    Solution

--- PuzzlOROctober2009.py
+++ PuzzlOROctober2009.py
@@ -1,9 +1,10 @@
 # PuzzlOR October 2009 Connected and Infected
 
+import numpy as np
 
-def update_infected_pop(infectionArea,infectionData,currentInfectionArea):    
+def calc_population(population_data):
+    return round(sum([sum(value) for value in population_data]),0)
 
-##population info
 constval = 10**9		
 currentBlue = 0.5 * constval		
 currentRed = 0.5 * constval		
@@ -13,39 +14,19 @@ currentGreen = 0.5 * constval
 currentPopulation = currentBlue + currentRed + currentViolet + currentYellow		
 + currentGreen
 
+population_data = [[10000,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+population_update = [[0.3,0.3,0.3,0,0],[0.3,0.3,0,0.3,0.3],[0.3,0,0.3,0,0],[0,0.3,0,0.3,0],
+                     [0,0.3,0,0,0.3]]
 
-
-infectionData = {		
- "blue":10000,		
- "violet":0,		
- "red":0,		
- "yellow":0,		
- "green":0		
- }
- 
-infectionArea = {
- "blue":["blue","violet","red"],		
- "violet":["violet","blue","green","yellow"],		
- "red":["red","blue"],		
- "yellow":["yellow","violet"],		
- "green":["green","violet"]	
- }
-
-currentInfectedArea = ["blue"]
-checkVal = False
-populationInfected = 0
-monthCount = 0
-
+checkVal = False                     
+monthCount = 0                 
 while not checkVal:
-    if populationInfected >= 0.5*currentPopulation:
+    if calc_population(np.array(population_data)) >= 0.5*currentPopulation:
         checkVal = True
         break
     else:
         monthCount+=1
-        currentInfectedArea = update_infected_pop(infectionArea,infectionData,currentInfectionArea)[0]
-        populationInfected = update_infected_pop(infectionArea,infectionData,currentInfectionArea)[1]
-        infectionArea = update_infected_pop(infectionArea,infectionData,currentInfectionArea)[2]
-        infectionData = update_infected_pop(infectionArea,infectionData,currentInfectionArea)[3] 
+        population_data = population_data + np.dot(np.array(population_data),np.matrix(population_update))
 print(monthCount)
 
 
