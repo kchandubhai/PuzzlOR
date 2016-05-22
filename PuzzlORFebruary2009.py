8c00b5a89ea3e8a145b28fa4f83cde36efe8303f
Edima Udo <edimaudo@gmail.com>
2016-05-22 04:41:50 -0400

    Update

--- PuzzlORFebruary2009.py
+++ PuzzlORFebruary2009.py
@@ -9,7 +9,7 @@ def find_closest(position,allPosition,positionUsed,xVal):
     for value in allPosition:
         if value not in positionUsed:
             positionData.append([value,euclidean_distance(generate_coordinate(position,xVal),generate_coordinate(value,xVal))])
-    sortedPosition = sorted(positionData,key=operator.itemgetter(0))   
+    sortedPosition = sorted(positionData,key=operator.itemgetter(1))       
     return sortedPosition[0]
                  
 def euclidean_distance(a,b):
@@ -24,60 +24,64 @@ def generate_coordinate(value,alist):
 def generate_area(xVal,yVal):
     return [str(x) + str(y) for x in xVal for y in yVal]
 
-def assign(factories,towns,xVal):
+def distanceCalc(factories,towns,xVal):
     assigned = []
     townAssigned = []
+    scores = []
     for factory in factories:
         checkVal = False
         while not checkVal:
             if len(townAssigned) == len(factories):
-              return (assigned)
+                  distance = sum([euclidean_distance
+                  (generate_coordinate(assigned[i][0],xVal),generate_coordinate(assigned[i][1],xVal)) * scores[i] *10 
+                  for i in range(len(scores))]) 
+                  return assigned
             closestTown = find_closest(factory,towns,townAssigned,xVal)
             townData = towns[closestTown[0]]
             factoryData = factories[factory]
-            if factoryData >= abs(townData):
+            if factoryData > abs(townData):
                 townAssigned.append(closestTown[0])
                 factories[factory]+=townData
+                scores.append(factories[factory])
                 towns[closestTown[0]] = 0
                 assigned.append([factory,closestTown[0]])
-            elif factoryData <= abs(townData):
+            elif factoryData < abs(townData):
+                scores.append(factoryData)
                 towns[closestTown[0]] += factoryData
                 factories[factory] = 0
                 assigned.append([factory,closestTown[0]])
                 checkVal = True
                 break
-    return (assigned)
-                
-
+            elif factoryData == abs(townData):
+                scores.append(factoryData)
+                assigned.append([factory,closestTown[0]])
+                townAssigned.append(closestTown[0])
+                factories[factory] = 0
+                towns[closestTown[0]] = 0
+                checkVal = True                
+                break
+              
 def main():
     xVal = ["A","B","C","D","E"]
     yVal = [1,2,3,4,5]
     area = generate_area(xVal,yVal)
-
+    
     factories = {"A1":500,"A5":1000,"C3":1500,"E2":1000}
     towns = {"A4":-500,"B2":-2000,"C4":-500,"E1":-1500,"E5":-500}
     factoryTownData = ["A1","A5","C3","E2","A4","B2","C4","E1","E5"]
-
+    
     possibleArea = [i for i in area if i not in factoryTownData]
     possibleAreaCapacity = 1000
-
-
+    
     outputList = []
     for area in possibleArea:
         temp = {}
         temp[area] = possibleAreaCapacity
         tempFactories = temp
         tempFactories.update(factories)             
-        outputList.append(assign(tempFactories,towns,xVal))
+        outputList.append(distanceCalc(tempFactories,towns,xVal))
+    outputList.sort
     print(outputList)
-        
-main()        
-
-        
-
-   
 
 
-
-
-
+main()
