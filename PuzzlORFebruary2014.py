8b49be187b5382140c33382e93ed59bf08cb6098
Edima Udo <edimaudo@gmail.com>
2016-05-23 21:52:37 -0400

    Update

--- PuzzlORFebruary2014.py
+++ PuzzlORFebruary2014.py
@@ -3,21 +3,23 @@
 import math
 import random
 
-    
-#assumption - a driver can only deliver one order per round trip
+def customerData():  
+    driverData = []
+    countVal = 0
+    maxCountVal = 100
+    while countVal < maxCountVal:
+        mandateDelivery = 60
+        customerInterArrivalTime = int(math.ceil(random.expovariate(0.1)))
+        customerCount = 0
+        driverCount = 0
+        while customerCount < customerInterArrivalTime:
+            pickupDelivery = int(math.ceil(random.uniform(20,60)))
+            if pickupDelivery <= mandateDelivery:
+                driverCount+=1
+            customerCount+=1
+        driverData.append(driverCount)
+        countVal+=1
+    print(round(sum(driverData)/maxCountVal,0))
 
-#How many drivers are needed in order to keep average delivery times under one hour?
+customerData()
 
-print(random.expovariate(0.1))
-#countVal = 0
-#maxCountVal = 10000
-#while countVal < 10000:
-#    mandateDelivery = 60
-#    pickupDelivery = random.uniform(20,60) 
-#    pickupDeliveryAverage = (20+60)/2
-#    pickupDeliveryVariance = math.pow(60-20,2)/12
-#    customerInterArrivalTime = 6 #hrs
-#    customerAverage = 1/customerInterArrivalTime
-#    customerVariance = math.pow(customerAverage,2)
-#    
-#    countVal+=1
