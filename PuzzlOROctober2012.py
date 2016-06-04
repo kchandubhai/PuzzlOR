fba72dd19fe2e67d0de914eeee23565bb5964287
Edima Udo <edimaudo@gmail.com>
2016-06-04 15:03:45 -0400

    Solution

--- PuzzlOROctober2012.py
+++ PuzzlOROctober2012.py
@@ -1,20 +1,10 @@
-""" Calculate the possibilities for the October 2012 PuzzlOR puzzle. """
+#PuzzlOR October 2012 FarmOR
 
 # information for each crop
 CORN = {'fertilizer_cost': 30000, 'profit': 190000,
         'insurance_cost': 35000, 'name': 'corn', 'loss': -190000}
 SOYBEANS = {'fertilizer_cost': 10000, 'profit': 170000,
             'insurance_cost': 20000, 'name': 'soybeans', 'loss': -170000}
-
-# all possible decision permutations
-COMBINATIONS = [(c, i, f)
-                for c in (CORN, SOYBEANS)
-                for i in (False, True)
-                for f in (False, True)]
-
-# format for output
-FORMAT = '$%0.2f \t (Plant: %s, Insurance: %s, Fertilizer: %s)'
-
 
 def prob_success(insurance, fertilizer):
     """ Return probability of success in harvesting the crop. """
@@ -26,8 +16,7 @@ def prob_success(insurance, fertilizer):
         return 0.95
     else:
         return 0.85
-
-
+            
 def expected_value(crop, insurance=0, fertilizer=0):
     """ Return expected value given success or failure. """
     p_success = prob_success(insurance, fertilizer)
@@ -36,12 +25,15 @@ def expected_value(crop, insurance=0, fertilizer=0):
     return profits - costs
 
 
-def get_all():
-    """ Return results for every decision permutation. """
+def main():
+    COMBINATIONS = [(c, i, f)
+                for c in (CORN, SOYBEANS)
+                for i in (False, True)
+                for f in (False, True)]
     for crop, insurance, fertilizer in COMBINATIONS:
-        expected = expected_value(crop, insurance, fertilizer)
-        print FORMAT % (expected, crop['name'], bool(insurance), bool(fertilizer))
+        expected = expected_value(crop, insurance,fertilizer)
+    print(expected,crop['name'], bool(insurance), bool(fertilizer))
 
 
-if __name__ == '__main__':
-    get_all()
+if __name__ == "__main__":
+    main()
