f531bbdedbc39c9df120c012b5bcc826aa3d8ba6
Edima Udo <edimaudo@gmail.com>
2016-06-04 18:08:30 -0400

    Update

--- PuzzlORJune2009.py
+++ PuzzlORJune2009.py
@@ -1,1 +1,28 @@
-#https://possiblywrong.wordpress.com/2010/12/26/why-multiple-checkout-lines-are-not-all-bad/
+#puzzlOR June 2009 Grocery queuing 
+
+import random
+import math
+
+
+def trad_queue(cashier,customerArrival,checkOut):
+    avg_waiting_time = 0
+    queue1 = []
+    queue2 = []
+    queue3 = []
+    
+    
+    
+    return avg_waiting_time
+
+def new_queue(cashier,customerArrival,checkOut):
+    avg_waiting_time = 0
+    return avg_waiting_time
+    
+    
+
+def main():
+    waiting_time_delta = trad_queue(3,2,5) - new_queue(1,2,5)
+    print(waiting_time_delta)
+
+if __name__ == "__main__":
+    main()
