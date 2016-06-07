c657e123ab55785fb0c8223b15accb729b88b597
Edima Udo <edimaudo@gmail.com>
2016-06-07 19:46:04 -0400

    Update

---
+++ PuzzlORDecember2013.py
@@ -0,0 +1,31 @@
+# PuzzloR December 2013 Golf queueing
+import random
+import math
+
+customerInterArrivalTime = 10
+player = {
+"slow":10,
+"medium":7, 
+"fast":5
+}
+
+def fifo_queue():
+    count = 0
+    maxcount = 10000
+    #while count < maxcount:
+    #    count+=1
+
+def priority_queue():        
+    count = 0
+    maxcount = 10000
+    #while count < maxcount:
+    #    count+=1
+
+def main():
+    fifo = fifo_queue()
+    priority = priority_queue()
+    time_delta - abs(fifo-priority)
+    print(time_delta)
+
+if __name__ == "__main__":
+    main()
