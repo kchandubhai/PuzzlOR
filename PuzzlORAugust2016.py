45fb62212c314d3a32f47b2a90acbf7e88d061f8
Edima Udo <edimaudo@gmail.com>
2016-07-24 17:58:10 -0400

    Update
    Data set up

---
+++ PuzzlORAugust2016.py
@@ -0,0 +1,39 @@
+# PuzzlOR Matchup August 2016
+
+
+#Person    Politics    Hobby    Living Preference    Social
+ManInfo = {
+"Man1":["Liberal","Running","Rural","Introverted"],
+"Man2":["Liberal","Chess","Rural","Outgoing"],
+"Man3":["Conservative","Running","Urban","Introverted"],
+"Man4":["Liberal","Running","Rural","Introverted"],
+"Man5":["Conservative","Running","Rural","Introverted"],
+"Man6":["Conservative","Chess","Rural","Introverted"],
+"Man7":["Conservative","Chess","Urban","Introverted"],
+"Man8":["Liberal","Chess","Urban","Introverted"],
+"Man9":["Liberal","Running","Urban","Introverted"],
+"Man10":["Liberal","Running","Urban","Outgoing"]
+}
+
+
+
+womenInfo = {
+"Woman1":["Conservative","Chess","Rural","Introverted"],
+"Woman2":["Liberal","Running","Rural","Introverted"],
+"Woman3":["Conservative","Chess","Rural","Outgoing"],
+"Woman4":["Conservative","Running","Urban","Outgoing"],
+"Woman5":["Conservative","Chess","Urban","Outgoing"],
+"Woman6":["Liberal","Running","Urban","Introverted"],
+"Woman7":["Conservative","Chess","Urban","Outgoing"],
+"Woman8":["Conservative","Running","Urban","Outgoing"],
+"Woman9":["Liberal","Chess","Urban","Introverted"],
+"Woman10":["Liberal","Running","Rural","Introverted"]
+}
+
+
+def main():
+    print("do something")
+
+
+if __name__ == __main__:
+    main()
