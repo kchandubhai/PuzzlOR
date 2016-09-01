5bfbdbde660f661f1017bf4852a5f19e52903325
Edima Udo <edimaudo@gmail.com>
2016-09-01 15:22:30 -0400

    Solution

--- PuzzlORAugust2015.py
+++ PuzzlORAugust2015.py
@@ -1,54 +1,65 @@
 # PuzzlOR August 2015 Moon Rover
 
 import math
-import random
 import itertools
-import operator
-from collections import Counter
-from operator import itemgetter
+import numpy as np
 
-def euclidean_distance(a,b):
-    c = [math.pow(a[i] - b[i],2) for i in range(len(a))]
-    return math.sqrt(sum(c))
-
-def calc_distance(alist,valueLookUp,distance):
-    sumval = 0
-    countval = 0
-    templist = []
-    scoreval = 0
-    while countval < len(alist)-1:
-        val1 = valueLookUp[alist[countval]]
-        val2 = valueLookUp[alist[countval + 1]]
-        tempval = sumval + euclidean_distance(val1[0],val2[0])
-        scoreval += val1[1]
-        if tempval >= distance:
-            break
-        sumval += euclidean_distance(val1[0],val2[0])
-        templist.append(alist[countval])
-        countval += 1
-    return [sumval,templist,scoreval]
-
-def main():
-    values = ["A8","B3","D10","E2","G3","G6","G8","H1",'H4',"I7"]
-    moonPosition = {
+def main:
+    site_name = ["A8","B3","D10","E2","G3","G6","G8","H1",'H4',"I7"]
+    site_location = np.array([[1,8],[2,3],[4,6],[4,10],[5,2],[7,3],[7,6],[7,8],
+                              [8,1],[8,4],[9,7]])
+    site_name_position = {
     "A8":[[1,8],3],"B3":[[2,3],3],"D6":[[4,6],0],"D10":[[4,10],5],
     "E2":[[5,2],4],"G3":[[7,3],5],"G6":[[7,6],4],"G8":[[7,8],2],
     "H1":[[8,1],2],'H4':[[8,4],1],"I7":[[9,7],1]
     }
-
-    finalist = []
-    distance = 25
-    maxDistance = 0
-    maxScore = 0
-    for arrangement in itertools.permutations(values):
-        temp = list(arrangement)
-        temp.insert(0,"D6")
-        temp1 = calc_distance(temp,moonPosition,distance)
-        if temp1[2] > maxScore:
-            del finalist[:]
-            maxScore = temp1[2]
-            finalist.append(temp1)        
-    print (finalist[2])
+    
+if __name__ == "__main__":
+    main()
+    
+#def euclidean_distance(a,b):
+#    c = [math.pow(a[i] - b[i],2) for i in range(len(a))]
+#    return math.sqrt(sum(c))
 
 
-main()
+#def calc_distance(alist,valueLookUp,distance):
+#    sumval = 0
+#    countval = 0
+#    templist = []
+#    scoreval = 0
+#    while countval < len(alist)-1:
+#        val1 = valueLookUp[alist[countval]]
+#        val2 = valueLookUp[alist[countval + 1]]
+#        tempval = sumval + euclidean_distance(val1[0],val2[0])
+#        scoreval += val1[1]
+#        if tempval >= distance:
+#            break
+#        sumval += euclidean_distance(val1[0],val2[0])
+#        templist.append(alist[countval])
+#        countval += 1
+#    return [sumval,templist,scoreval]
+#
+#def main():
+#    values = ["A8","B3","D10","E2","G3","G6","G8","H1",'H4',"I7"]
+#    moonPosition = {
+#    "A8":[[1,8],3],"B3":[[2,3],3],"D6":[[4,6],0],"D10":[[4,10],5],
+#    "E2":[[5,2],4],"G3":[[7,3],5],"G6":[[7,6],4],"G8":[[7,8],2],
+#    "H1":[[8,1],2],'H4':[[8,4],1],"I7":[[9,7],1]
+#    }
+#
+#    finalist = []
+#    distance = 25
+#    maxDistance = 0
+#    maxScore = 0
+#    for arrangement in itertools.permutations(values):
+#        temp = list(arrangement)
+#        temp.insert(0,"D6")
+#        temp1 = calc_distance(temp,moonPosition,distance)
+#        if temp1[2] > maxScore:
+#            del finalist[:]
+#            maxScore = temp1[2]
+#            finalist.append(temp1)        
+#    print (finalist[2])
+
+
+
--- PuzzlORFebruary2016.py
+++ PuzzlORFebruary2016.py
@@ -1,38 +1,20 @@
 # PuzzlOR February 2016 Toy Builder
-import math
-import random
 
-def generate_random(alist):
-    return (random.randint(0,len(alist)-1))
+def main():
+    profit_list = []
+    #A - airplane, H-Helicpoter, C-Car
+    for A in range(9):
+        for H in range(8):
+            for C in range(8):
+                Profit = 7*A + 8*H + 5*C
+                Blue = 3*A + 2*H + C
+                Green = 2*A + 4*H + 2*C
+                Red = A + H + 4*C
+                if (Blue <= 25 and Green <= 29 and Red <= 30):
+                    profit_list.append(Profit)
+    print(max(profit_list))
+    
+if __name__ == "__main__":
+    main()
 
-def update_part(parts,toy):
-    return [parts[i] - toy[i] for i in range(len(parts))]
 
-def check_valid(parts,toy):
-    output = []
-    for i in range(len(parts)):
-        if parts[i] < toy[i]:
-            output.append(i)
-    if len(output) > 0:
-        return True
-    return False
-
-maxProfit = []
-countval = 0
-maxcount = 10000
-while countval < maxcount:
-    profit = 0
-    partAvailable = [25,29,30]
-    toyBuilder = [['airplane',[3,2,1],7],['helicopter',[2,4,1],8],['car',[1,2,4],5]]
-    checkAvailable = False
-    while not checkAvailable:
-        toy = toyBuilder[generate_random(toyBuilder)]
-        if check_valid(partAvailable,toy[1]):
-            checkAvailable = True
-            break
-        partAvailable = update_part(partAvailable,toy[1])
-        profit += toy[2]
-    maxProfit.append(profit)
-    countval += 1
-maxProfit.sort()
-print(max(maxProfit))
