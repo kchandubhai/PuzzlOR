87ad898d85dda489f8bcc094920ab77265d9073d
Edima Udo <edimaudo@gmail.com>
2016-09-01 16:53:57 -0400

    Solution

--- PuzzlORAugust2015.py
+++ PuzzlORAugust2015.py
@@ -1,65 +1,43 @@
 # PuzzlOR August 2015 Moon Rover
 
-import math
 import itertools
 import numpy as np
+from scipy.spatial.distance import pdist, squareform
 
-def main:
-    site_name = ["A8","B3","D10","E2","G3","G6","G8","H1",'H4',"I7"]
-    site_location = np.array([[1,8],[2,3],[4,6],[4,10],[5,2],[7,3],[7,6],[7,8],
-                              [8,1],[8,4],[9,7]])
-    site_name_position = {
-    "A8":[[1,8],3],"B3":[[2,3],3],"D6":[[4,6],0],"D10":[[4,10],5],
-    "E2":[[5,2],4],"G3":[[7,3],5],"G6":[[7,6],4],"G8":[[7,8],2],
-    "H1":[[8,1],2],'H4':[[8,4],1],"I7":[[9,7],1]
-    }
+def main():
     
-if __name__ == "__main__":
-    main()
-    
-#def euclidean_distance(a,b):
-#    c = [math.pow(a[i] - b[i],2) for i in range(len(a))]
-#    return math.sqrt(sum(c))
-
-
-#def calc_distance(alist,valueLookUp,distance):
-#    sumval = 0
-#    countval = 0
-#    templist = []
-#    scoreval = 0
-#    while countval < len(alist)-1:
-#        val1 = valueLookUp[alist[countval]]
-#        val2 = valueLookUp[alist[countval + 1]]
-#        tempval = sumval + euclidean_distance(val1[0],val2[0])
-#        scoreval += val1[1]
-#        if tempval >= distance:
-#            break
-#        sumval += euclidean_distance(val1[0],val2[0])
-#        templist.append(alist[countval])
-#        countval += 1
-#    return [sumval,templist,scoreval]
-#
-#def main():
-#    values = ["A8","B3","D10","E2","G3","G6","G8","H1",'H4',"I7"]
-#    moonPosition = {
+    #site_name = ["A8","B3","D10","E2","G3","G6","G8","H1",'H4',"I7"]
+#    site_name_position = {
 #    "A8":[[1,8],3],"B3":[[2,3],3],"D6":[[4,6],0],"D10":[[4,10],5],
 #    "E2":[[5,2],4],"G3":[[7,3],5],"G6":[[7,6],4],"G8":[[7,8],2],
 #    "H1":[[8,1],2],'H4':[[8,4],1],"I7":[[9,7],1]
 #    }
-#
-#    finalist = []
-#    distance = 25
-#    maxDistance = 0
-#    maxScore = 0
-#    for arrangement in itertools.permutations(values):
-#        temp = list(arrangement)
-#        temp.insert(0,"D6")
-#        temp1 = calc_distance(temp,moonPosition,distance)
-#        if temp1[2] > maxScore:
-#            del finalist[:]
-#            maxScore = temp1[2]
-#            finalist.append(temp1)        
-#    print (finalist[2])
+    site_location = np.array([[1,8],[2,3],[4,6],[4,10],[5,2],[7,3],[7,6],[7,8],
+                              [8,1],[8,4],[9,7]])             
+    site_score = [3,3,0,5,4,5,4,2,2,1,1]
+    site_euclidean_distance = (squareform(pdist(site_location,"euclidean")))
+    max_path_value = 0
+    for path in itertools.permutations([1,2,3,4,5,6,7,8,9,10]):
+        site = 0
+        path_length = 0.00
+        path_value = 0
+        for site_data in path:
+            distance = site_euclidean_distance[site][site_data]
+            if(path_length+distance > 25.0):
+                break
+            else:
+                path_length+=distance
+                site = site_data
+                path_value += site_score[site]
+        if(path_value > max_path_value):
+            max_path_value = path_value
+            best_path = path
+    print(max_path_value,best_path)
+    
+if __name__ == "__main__":
+    main()
+    
+
 
 
 
