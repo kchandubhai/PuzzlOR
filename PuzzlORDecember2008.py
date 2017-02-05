73cca29550d6a9f70be8bbc844f0732330c990ca
Edima Udo <edimaudo@gmail.com>
2017-02-05 13:15:50 -0500

    Not solved

---
+++ PuzzlORDecember2008.py
@@ -0,0 +1,26 @@
+#import math
+#import random
+#
+#def calc_avg(alist):
+#    return math.ceil(sum(alist)/len(alist))
+#
+#def customerData():
+#    driverData = []
+#    countVal = 0
+#    maxCountVal = 60
+#    mandateDelivery = 60
+#    while countVal < maxCountVal:
+#        orderInterArrivalTime = 6
+#        orderArrival = math.ceil(random.expovariate(1/orderInterArrivalTime))
+#        orderCount = 0
+#        driverCount = 0
+#        pickupDeliveryList = []
+#        while orderCount < orderArrival:
+#            pickupDelivery = math.ceil(random.uniform(19,60))
+#            pickupDeliveryList.append(pickupDelivery)
+#            if calc_avg(pickupDeliveryList) < mandateDelivery:
+#                driverCount+=1
+#            orderCount+=1
+#        driverData.append(driverCount)
+#        countVal+=1
+#    return(calc_avg(driverData))
