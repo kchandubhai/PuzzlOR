a6474b1a7c867630bc48fcf0ad3e0bda45ed101a
Edima Udo <edimaudo@gmail.com>
2017-03-06 19:47:34 -0500

    update

--- PuzzlORFebruary2017.py
+++ PuzzlORFebruary2017.py
@@ -1,32 +1,9 @@
 #PuzzlOR February 2017 Any Port In A Storm
 #minimum possible total distance traveled by all boats
-import sys
 
 
-def main():
-	try:
-		cluster_check = [0,0,0]
-		distance = 0
-		dock_size = [4,7,9]
-		#ships, distance
-		cluster_info = {
-			1:[8,[1,3,2]],
-			2:[5,[2,2,1]],
-			3:[7,[3,2,1]]
-		}
 
 
-		
-	except:
-		e = sys.exc_info()
-		print(e)
-		sys.exit()
-
-if __name__ == "__main__":
-	main()
-
-
-"""
 #PuzzlOR February 2017 Any Port In A Storm
 #minimum possible total distance traveled by all boats
 import sys
@@ -67,5 +44,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
-"""
